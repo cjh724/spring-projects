83f1dd2c2dab70ba23ed76a5017f64c46a3d522d
cjh724 <jh.cho133104@gmail.com>
2020-03-21 23:17:36 +0900

    Init RestController example

---
+++ example01/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,13 @@
+package org.zerock.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+}
