620b490928c4cb70e580c51f2e91b6067eaf89ae
cjh724 <jh.cho133104@gmail.com>
2020-04-25 23:25:55 +0900

    Add reply test using ajax(~p423)

--- example01/src/main/java/org/zerock/controller/HomeController.java
+++ example01/src/main/java/org/zerock/controller/HomeController.java
@@ -1,15 +1,12 @@
 package org.zerock.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.text.*;
+import java.util.*;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Handles requests for the application home page.
@@ -36,4 +33,8 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public void ajaxTest() {
+		
+	}
 }
