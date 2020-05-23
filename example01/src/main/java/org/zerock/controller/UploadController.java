3f4d7ed121468841d48d18273d546f2b46425bc8
cjh724 <jh.cho133104@gmail.com>
2020-05-23 18:37:27 +0900

    Add fileupload(~p535)

---
+++ example01/src/main/java/org/zerock/controller/UploadController.java
@@ -0,0 +1,50 @@
+package org.zerock.controller;
+
+import java.io.*;
+import java.util.*;
+
+import javax.annotation.*;
+
+import org.slf4j.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.util.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.*;
+
+@Controller
+@RequestMapping("/fileupload")
+public class UploadController {
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+	@Resource(name="uploadPath")
+	private String uploadPath;
+	
+	@RequestMapping(value="/uploadForm", method=RequestMethod.GET)
+	public void uploadForm() {
+		
+	}
+	
+	@RequestMapping(value="/uploadForm", method=RequestMethod.POST)
+	public String uploadForm(MultipartFile file, Model model) throws Exception {
+		logger.info("originalName : " + file.getOriginalFilename());
+		logger.info("size : " + file.getSize());
+		logger.info("contentType : " + file.getContentType());
+		
+		String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
+		
+		model.addAttribute("savedName", savedName);
+		
+		return "fileupload/uploadResult";
+	}
+	
+	private String uploadFile(String originalName, byte[] fileData) throws Exception {
+		UUID uid = UUID.randomUUID();
+		String savedName = uid.toString() + "_" + originalName;
+		File target = new File(uploadPath, savedName);
+		FileCopyUtils.copy(fileData, target);
+		
+		return savedName;
+	}
+
+}
