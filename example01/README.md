0fae5dedc81dce67b63a3587e1f46f21c81ac98f
cjh724 <jh.cho133104@gmail.com>
2020-07-04 15:04:05 +0900

    Add logout(~p675)

--- example01/README.md
+++ example01/README.md
@@ -1,3 +1,4 @@
 * URL 정리
-1. http://localhost:8088/user/login
-2. http://localhost:8088/sboard/list
+1. 로그인 : http://localhost:8088/user/login
+2. 로그아웃 : http://localhost:8088/user/loguot
+3. 게시판 : http://localhost:8088/sboard/list
--- example01/src/main/java/org/zerock/controller/UserController.java
+++ example01/src/main/java/org/zerock/controller/UserController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.*;
 import org.zerock.domain.*;
 import org.zerock.dto.*;
 import org.zerock.service.*;
@@ -49,6 +50,29 @@ public class UserController {
 			
 			service.keepLogin(vo.getUid(), session.getId(), sessionLimit);
 		}
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public String logout(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
+		Object obj = session.getAttribute("login");
+		
+		if(obj != null) {
+			UserVO vo = (UserVO) obj;
+			
+			session.removeAttribute("login");
+			session.invalidate();	// 생성된 세션 무효화(소멸 아님)
+			
+			Cookie loginCookie = WebUtils.getCookie(request, "loginCookie");
+			
+			if(loginCookie != null) {
+				loginCookie.setPath("/");
+				loginCookie.setMaxAge(0);
+				response.addCookie(loginCookie);
+				service.keepLogin(vo.getUid(), session.getId(), new Date());
+			}
+		}
+		
+		return "user/logout";
 	}
 	
 }
